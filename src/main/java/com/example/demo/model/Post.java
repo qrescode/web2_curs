7d1b9e4a02f1a02fffa8acdb4475920d7c0bb14e
Your Name <your@email.com>
2026-02-11 23:14:33 +0500

    first commit

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,18 +1,26 @@
 package com.example.demo.model;
 
-public class Post {
-    private String text;
-    private Integer likes;
+import java.util.Date;
 
-    public Post(String text) {
+public class Post {
+    private final String text;
+    private Integer likes;
+    private Date creationDate;
+
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
+        this.likes = 0;
     }
 
     public String getText() {
         return text;
     }
-
     public Integer getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,19 +3,17 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
 
     public List<Post> listAllPosts() {
-        return Arrays.asList(
-                new Post("Первый пост!"),
-                new Post("1"),
-                new Post("2"),
-                new Post("2")
-
+        return List.of(
+                new Post("Всем привет! Это мой пост",new Date()),
+                new Post("Hi Im a student!",new  Date()),
+                new Post("I work with java!",new Date())
         );
     }
 }
